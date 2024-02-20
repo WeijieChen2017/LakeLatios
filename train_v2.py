cb4f37a73f545e349245fb7b92546b185c9200c6
WeijieChen2017 <weijiechen1994@gmail.com>
2024-02-19 19:14:26 -0600

    Good morning!

--- train_v2.py
+++ train_v2.py
@@ -99,7 +99,6 @@ for epoch in range(cfg["epochs"]):
     for batch in train_loader:
         MR = batch["MR"]
         CT = batch["CT"]
-        print(MR.shape, CT.shape)
         MR = MR.to(device)
         CT = CT.to(device)
         optimizer.zero_grad()
