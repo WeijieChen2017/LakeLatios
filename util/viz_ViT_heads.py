d0828de0b2cb99d2a9ba6ea30c05289f5c610f22
WeijieChen2017 <weijiechen1994@gmail.com>
2024-02-21 16:35:33 -0600

    Good morning!

--- util/viz_ViT_heads.py
+++ util/viz_ViT_heads.py
@@ -14,6 +14,8 @@ def viz_ViT_heads(ViT_heads, save_path):
 
     # save all intensity images in one figure
     fig, axs = plt.subplots(3, 5, figsize=(20, 12))
+    # head_names = [f'patch_emb', f'pos_emb', f'head {i}' for i in range(12), f'neck']
+    head_names = ['patch_emb', 'pos_emb', 'head 0', 'head 1', 'head 2', 'head 3', 'head 4', 'head 5', 'head 6', 'head 7', 'head 8', 'head 9', 'head 10', 'head 11', 'neck']
     for i in range(3):
         for j in range(5):
             data = ViT_heads[i*5+j]
@@ -21,20 +23,41 @@ def viz_ViT_heads(ViT_heads, save_path):
             data = np.squeeze(np.mean(data, axis=-1))
             axs[i, j].imshow(data, cmap='gray')
             axs[i, j].axis('off')
-            axs[i, j].set_title(f'head {i*5+j}')
+            axs[i, j].set_title(head_names[i*5+j])
     plt.savefig(save_path+"/intensity.png")
     plt.close()
 
     # plot the data distribution in one column
-    fig, axs = plt.subplots(15, 1, figsize=(5, 30))
+    fig, axs = plt.subplots(15, 1, figsize=(5, 40))
     for i in range(15):
         data = ViT_heads[i]
         data = np.squeeze(np.mean(data, axis=-1))
         axs[i].hist(data.flatten(), bins=100)
-        axs[i].set_title(f'head {i}')
+        axs[i].set_title(head_names[i])
+        # set the range from -1 to 1
+        axs[i].set_xlim(-1, 1)
     plt.savefig(save_path+"/data_distribution.png")
     plt.close()
 
+    # plot the cosine similarity in one column
+    for i in range(15):
+
+        data = ViT_heads[i]
+        # 64, 64, emb to 64*64, emb
+        data = np.reshape(data, (64*64, -1))
+        px_cos_sim = np.zeros((64*64, 64*64))
+        for j in range(64*64):
+            for k in range(64*64):
+                px_cos_sim[j, k] = np.dot(data[j], data[k]) / (np.linalg.norm(data[j]) * np.linalg.norm(data[k]))
+        
+        fig, axs = plt.subplots(1, 1, figsize=(5, 5))
+        axs.imshow(px_cos_sim, cmap='gray')
+        axs.set_title(head_names[i])
+        plt.savefig(save_path+f"/cosine_similarity_{i}.png")
+        plt.close()
+
     
+
+
 
 
