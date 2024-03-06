ef43dd8dd8493fd6dc992f62537b5b58c8d239a8
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-05 19:17:30 -0600

    Good morning!

--- notebook.py
+++ notebook.py
@@ -6,3 +6,26 @@
 # eval_1 -> GPU:2
 # eval_2 -> GPU:6
 # eval_3 -> GPU:7
+
+# load from checkpoint ./proj/decoder_UNETR/best_model.pth
+# Start testing on 2233 samples.
+# Test Loss: 38.6859182846043
+
+# load from checkpoint ./proj/decoder_Deconv/best_model.pth
+# Start testing on 2233 samples.
+# Test Loss: 44.63018013609501
+
+# load from checkpoint ./proj/decoder_PyramidPooling/model_270.pth
+# Start testing on 2233 samples.
+# Test Loss: 39.183466535143346
+
+# load from checkpoint ./proj/decoder_PP_pct20/best_model.pth
+# Start testing on 2233 samples.
+# Test Loss: 37.103882594512875
+
+# load from checkpoint ./proj/decoder_PP_pct100/best_model.pth
+# Start testing on 2233 samples.
+# Test Loss: 36.82605276931957
+
+# eval_31 -> GPU:7
+# eval_32 -> GPU:7
