680afb0ecdc4bc7aa9ebb134bc04bca56846bc5f
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-05 20:07:05 -0600

    Good morning!

--- util_rebuild_nifty.py
+++ util_rebuild_nifty.py
@@ -1,6 +1,6 @@
 # load all nifty in "./data/nifty/CT/"
-save_folder = "./results/test_Deconv_v1_nifty/"
-pred_folder = "./results/test_Deconv_v1/"
+save_folder = "./results/nifiti_test_PP_pct20_v1/"
+pred_folder = "./results/test_PP_pct20_v1/"
 
 import numpy as np
 import os
