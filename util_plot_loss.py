212e9a0853194bdc3dd9c50e69c21e68a6953f2d
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-06 12:53:39 -0600

    Good morning!

--- util_plot_loss.py
+++ util_plot_loss.py
@@ -77,8 +77,8 @@ def plot_loss_from_file(filename, output_tag):
         print(f"Start epoch: {start_epoch}, end epoch: {end_epoch}")
         while len(np.where(loss_model[idx_itpl, end_epoch:] > 0)[0] > 0):
             if end_epoch == start_epoch + 1:
-                start_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][0]
-                end_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][1]
+                start_epoch = np.where(loss_model[idx_itpl, end_epoch+1:] > 0)[0][0]
+                end_epoch = np.where(loss_model[idx_itpl, end_epoch+1:] > 0)[0][1]
             else:
                 # we use quadratic interpolation between start_epoch and end_epoch
                 point_1 = (start_epoch, loss_model[idx_itpl, start_epoch])
@@ -86,8 +86,8 @@ def plot_loss_from_file(filename, output_tag):
                 print(f"Interpolate {idx_itpl} from {start_epoch}[{loss_model[idx_itpl, start_epoch]}] to {end_epoch}[{loss_model[idx_itpl, end_epoch]}]")
                 for idx in range(start_epoch+1, end_epoch):
                     loss_model[idx_itpl, idx] = np.interp(idx, [point_1[0], point_2[0]], [point_1[1], point_2[1]])
-                start_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][0]
-                end_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][1]
+                start_epoch = np.where(loss_model[idx_itpl, end_epoch+1:] > 0)[0][0]
+                end_epoch = np.where(loss_model[idx_itpl, end_epoch+1:] > 0)[0][1]
 
     # plot the loss, label is the folder name
     plt.figure(figsize=(10, 5), dpi=100)
