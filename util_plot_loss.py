f8e4efe2c428165a5808950d2b64a66553e2b90a
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-06 12:43:40 -0600

    Good morning!

--- util_plot_loss.py
+++ util_plot_loss.py
@@ -68,16 +68,12 @@ def plot_loss_from_file(filename, output_tag):
     # given the valid epoch, we can interpolate the loss
     # if there is zero before the first valid epoch, we can interpolate the loss
     for idx_itpl in list_interpolate:
-        start_epoch = 0
-        end_epoch = np.where(loss_model[idx_itpl, :] > 0)[0][0]
-        if end_epoch == 0:
-            end_epoch = 1
+        start_epoch = np.where(loss_model[idx_itpl, :] > 0)[0][0]
+        end_epoch = np.where(loss_model[idx_itpl, :] > 0)[0][1]
         while end_epoch < vaild_epoch[idx_itpl]:
             if end_epoch == start_epoch + 1:
-                start_epoch = end_epoch
-                end_epoch = np.where(loss_model[idx_itpl, start_epoch:] > 0)[0][0] + start_epoch
-                if end_epoch == start_epoch:
-                    end_epoch += 1
+                start_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][0]
+                end_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][1]
             else:
                 # we use quadratic interpolation between start_epoch and end_epoch
                 point_1 = (start_epoch, loss_model[idx_itpl, start_epoch])
@@ -85,10 +81,8 @@ def plot_loss_from_file(filename, output_tag):
                 print(f"Interpolate {idx_itpl} from {start_epoch}[{loss_model[idx_itpl, start_epoch]}] to {end_epoch}[{loss_model[idx_itpl, end_epoch]}]")
                 for idx in range(start_epoch+1, end_epoch):
                     loss_model[idx_itpl, idx] = np.interp(idx, [point_1[0], point_2[0]], [point_1[1], point_2[1]])
-                start_epoch = end_epoch
-                end_epoch = np.where(loss_model[idx_itpl, start_epoch:] > 0)[0][0] + start_epoch
-                if end_epoch == start_epoch:
-                    end_epoch += 1
+                start_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][0]
+                end_epoch = np.where(loss_model[idx_itpl, end_epoch:] > 0)[0][1]
 
     # plot the loss, label is the folder name
     plt.figure(figsize=(10, 5), dpi=100)
