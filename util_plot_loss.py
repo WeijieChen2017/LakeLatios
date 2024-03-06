02ea2121d0c6f6ce66547445e709e2a338a71f6b
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-05 21:43:11 -0600

    Good morning!

--- util_plot_loss.py
+++ util_plot_loss.py
@@ -69,7 +69,7 @@ def plot_loss_from_file(filename, output_tag):
     for idx in range(n_model):
         label = list_model[idx].split("/")[-1]
         data = loss_model[idx, :vaild_epoch[idx]]
-        plt.plot(range(vaild_epoch[idx]), data, label=label, s=1, alpha=0.5)
+        plt.plot(range(vaild_epoch[idx]), data, label=label, alpha=0.5)
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.title(output_tag + " Loss")
