cc7a20330790e785c7f8bd56181a865dd5c207d1
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-05 21:25:38 -0600

    Good morning!

--- util_plot_loss.py
+++ util_plot_loss.py
@@ -26,17 +26,19 @@ list_w_timestamp = [
 ]
 
 # in loss.txt
-# each line in list_wo_timestamp is like Epoch 1/300, loss: 4.489071458006995e-07
 # we need to extract the epoch and loss
 
 n_wo_timestamp = len(list_wo_timestamp)
+n_w_timestamp = len(list_w_timestamp)
+n_model = n_wo_timestamp + n_w_timestamp
 max_epoch = 300
-loss_wo_timestamp = np.zeros((n_wo_timestamp, max_epoch))
-vaild_epoch = np.zeros(n_wo_timestamp, dtype=int)
+loss_wo_timestamp = np.zeros((n_model, max_epoch))
+vaild_epoch = np.zeros(n_model, dtype=int)
+
+# each line in list_wo_timestamp is like Epoch 1/300, loss: 4.489071458006995e-07
 for idx, folder in enumerate(list_wo_timestamp):
     loss_file = folder + "/loss.txt"
     model_name = folder.split("/")[-1]
-    print(f"Loading {loss_file} for {model_name}")
     with open(loss_file, "r") as f:
         lines = f.readlines()
         for line in lines:
@@ -46,13 +48,24 @@ for idx, folder in enumerate(list_wo_timestamp):
             loss = float(loss.split(": ")[1])
             loss_wo_timestamp[idx, epoch-1] = loss
             vaild_epoch[idx] = epoch
-            print(f"Epoch {epoch}, loss: {loss}")
 
-print(vaild_epoch)
+# each line in list_w_timestamp is like %2024-03-05 22:15:29% -> Epoch 1/300, loss: 0.0001564910279885503
+for idx, folder in enumerate(list_w_timestamp):
+    loss_file = folder + "/loss.txt"
+    model_name = folder.split("/")[-1]
+    with open(loss_file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            epoch, loss = line.split(", ")
+            # ValueError: could not convert string to float: 'loss: 7.096514131411594e-06\n'
+            epoch = int(epoch.split(" ")[1].split("/")[0])
+            loss = float(loss.split(": ")[1])
+            loss_wo_timestamp[n_wo_timestamp+idx, epoch-1] = loss
+            vaild_epoch[n_wo_timestamp+idx] = epoch
 
 # plot the loss, label is the folder name
 plt.figure(figsize=(10, 5), dpi=100)
-for idx in range(n_wo_timestamp):
+for idx in range(n_model):
     label = list_wo_timestamp[idx].split("/")[-1]
     data = loss_wo_timestamp[idx, :vaild_epoch[idx]]
     plt.plot(data, label=label)
@@ -61,6 +74,8 @@ plt.ylabel("Loss")
 plt.title("Training Loss")
 save_name = "training_loss_wo_timestamp.png"
 plt.legend()
+plt.yscale("log")
 plt.savefig(save_name)
 plt.close()
 print(f"Save {save_name}")
+
