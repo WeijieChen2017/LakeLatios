9f9c907947121503423d6061a50e66126fa68979
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-09 01:58:22 -0600

    Good morning!

---
+++ util_test_load_small_hdf5.py
@@ -0,0 +1,19 @@
+# given three hdf5 files, compare the loading time
+
+import os
+import h5py
+import glob
+import time
+
+file_list = [
+    "data/MIMRTL_Brain/00001/*"
+]
+slice_list = sorted(glob.glob(os.join(file_list[0], "pack*.hdf5")))
+
+for file_path in file_list:
+    print(f"Loading {file_path}")
+    start = time.time()
+    with h5py.File(file_path, "r") as f:
+        print(f"Keys: {list(f.keys())}")
+    print(f"Loading {file_path} takes {time.time() - start:.2f} seconds")
+    print()
