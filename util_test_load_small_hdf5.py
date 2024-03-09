24ae9b20e323bdd75ac83860d71069e81db71385
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-09 02:03:43 -0600

    Good morning!

--- util_test_load_small_hdf5.py
+++ util_test_load_small_hdf5.py
@@ -20,3 +20,9 @@ for slice_path in slice_list:
         print(f"Modalities: {modalities}", end="")
     print(f"Loading {slice_path} takes {time.time() - start:.2f} seconds")
     print()
+
+# Loading data/MIMRTL_Brain/00001/pack_121.hdf5Keys: ['97']Modalities: ['ct', 'mr', 'mr_emb_head_12', 'mr_emb_head_3', 'mr_emb_head_6', 'mr_emb_head_9', 'mr_emb_head_neck']Loading data/MIMRTL_Brain/00001/pack_121.hdf5 takes 0.00 seconds
+
+# Loading data/MIMRTL_Brain/00001/pack_122.hdf5Keys: ['98']Modalities: ['ct', 'mr', 'mr_emb_head_12', 'mr_emb_head_3', 'mr_emb_head_6', 'mr_emb_head_9', 'mr_emb_head_neck']Loading data/MIMRTL_Brain/00001/pack_122.hdf5 takes 0.01 seconds
+
+# Loading data/MIMRTL_Brain/00001/pack_123.hdf5Keys: ['99']Modalities: ['ct', 'mr', 'mr_emb_head_12', 'mr_emb_head_3', 'mr_emb_head_6', 'mr_emb_head_9', 'mr_emb_head_neck']Loading data/MIMRTL_Brain/00001/pack_123.hdf5 takes 0.02 seconds
