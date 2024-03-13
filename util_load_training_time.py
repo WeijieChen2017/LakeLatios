9ba2dfb3ec708e79472c6291b8fcf0236190ed9a
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-13 11:04:56 -0500

    Good morning!

--- util_load_training_time.py
+++ util_load_training_time.py
@@ -3,35 +3,39 @@ import time
 import glob
 import numpy as np
 
-directory_path_list = [
-    "proj/decoder_Deconv",
-    "proj/decoder_PP_pct100",
-    "proj/decoder_PP_pct20",
-    "proj/decoder_PyramidPooling",
-    "proj/decoder_UNetMONAI",
-    "proj/decoder_UNetMONAI_pct100",
-    "proj/decoder_UNetMONAI_pct20",
-    "proj/decoder_UNetMONAI_pct5",
-    "proj/decoder_UNETR",
-    "proj/decoder_UNETR_pct100",
-    "proj/decoder_UNETR_pct20",
-    "proj/MR2CT_InstanceUNet",
-    "proj/original_conv_output_PP_ViTheads_encoder_MedSAM",
-    "proj/original_conv_output_ViTheads_encoder_MedSAM",
-    "proj/output_PP_ViTheads_encoder_MedSAM",
-    "proj/output_UNETR_IN_ViTheads_encoder_MedSAM",
-    "proj/output_UNETR_ViTheads_encoder_MedSAM",
-    "proj/prembed_Deconv_pct5_MB",
-    "proj/prembed_PP_pct5_MB",
-    "proj/prembed_UNETR_pct5_MB",
-    "proj/small_prembed_Deconv_pct5_MB",
-    "proj/small_prembed_PP_pct5_MB",
-    "proj/small_prembed_UNETR_pct5_MB",
-    "proj/UNetMONAI_pct100",
-    "proj/UNetMONAI_pct100_43",
-    "proj/UNetMONAI_pct20",
-    "proj/UNetMONAI_pct5",
-]
+# directory_path_list = [
+#     "proj/decoder_Deconv",
+#     "proj/decoder_PP_pct100",
+#     "proj/decoder_PP_pct20",
+#     "proj/decoder_PyramidPooling",
+#     "proj/decoder_UNetMONAI",
+#     "proj/decoder_UNetMONAI_pct100",
+#     "proj/decoder_UNetMONAI_pct20",
+#     "proj/decoder_UNetMONAI_pct5",
+#     "proj/decoder_UNETR",
+#     "proj/decoder_UNETR_pct100",
+#     "proj/decoder_UNETR_pct20",
+#     "proj/MR2CT_InstanceUNet",
+#     "proj/original_conv_output_PP_ViTheads_encoder_MedSAM",
+#     "proj/original_conv_output_ViTheads_encoder_MedSAM",
+#     "proj/output_PP_ViTheads_encoder_MedSAM",
+#     "proj/output_UNETR_IN_ViTheads_encoder_MedSAM",
+#     "proj/output_UNETR_ViTheads_encoder_MedSAM",
+#     "proj/prembed_Deconv_pct5_MB",
+#     "proj/prembed_PP_pct5_MB",
+#     "proj/prembed_UNETR_pct5_MB",
+#     "proj/small_prembed_Deconv_pct5_MB",
+#     "proj/small_prembed_PP_pct5_MB",
+#     "proj/small_prembed_UNETR_pct5_MB",
+#     "proj/UNetMONAI_pct100",
+#     "proj/UNetMONAI_pct100_43",
+#     "proj/UNetMONAI_pct20",
+#     "proj/UNetMONAI_pct5",
+# ]
+
+# load every folder in proj which has something like proj/*/epoch_*.png
+directory_path_list = sorted(glob.glob("proj/*/"))
+
 
 # Dictionary to hold file names and their modified times
 file_modified_times = {}
