179a8061492194765fa0611e1e65ed320e602522
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-21 16:03:45 -0500

    Good morning!

--- util_clean_dataset.py
+++ util_clean_dataset.py
@@ -14,18 +14,13 @@ print("Found", len(data_list), "data files")
 input("Press Enter to continue...")
 
 for idx, data_path in enumerate(data_list):
-    print(f"Processing {idx+1}/{len(data_list)}: {data_path}")
+    # print(f"Processing {idx+1}/{len(data_list)}: {data_path}")
     with h5py.File(data_path, 'r') as f:
         # get keys
         keys = list(f.keys())
         # get data
         for key in keys:
             data = f[key][()]
-            print(key, np.sum(data < 1e-3), np.mean(data), np.std(data))
-            # check whether the data(ndarray) is valid, if not, change the filename to .invalid
-            # rule: the num of values < 1e-3 should be less than 1/1000 of the total num of values
-            # if np.sum(data < 1e-3) > data.size / 1000:
-            #     print(f"---------> Invalid data: {data_path}")
-            #     os.system(f"mv {data_path} {data_path}.invalid")
-
+            if np.mean(data) < 1e-3:
+                print(f"Warning: {key} in {data_path} has mean {np.mean(data)}")
             
