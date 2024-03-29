1876fb0857649f48d64e14f3f45217c835dd771d
WeijieChen2017 <weijiechen1994@gmail.com>
2024-03-29 04:34:07 -0500

    Good morning!

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -2,3 +2,4 @@ from .paired_MR_CT import PairedMRCTDataset_train
 from .paired_MR_CT import PairedMRCTDataset_test
 from .small_hdf5 import small_hdf5_dataset
 from .small_hdf5 import slice_hdf5_dataset
+from .small_npy import slice_npy
---
+++ dataset/small_npy.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+class slice_npy(Dataset):
+    def __init__(self, file_dict_list, required_keys, transform=None):
+        """
+        Args:
+            file_paths (list of str): List of npy file paths.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.file_dict_list = file_dict_list
+        self.required_keys = required_keys
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_path_list)
+
+    def __getitem__(self, idx):
+        data = {}
+        for key in self.required_keys:
+            data[key] = np.load(self.file_dict_list[idx][key], allow_pickle=True)
+
+        return data
