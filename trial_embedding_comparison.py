5d2bacabb99137e307eb93b8a121fc44e98ea455
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-02 01:54:15 -0500

    Good morning!

--- trial_embedding_comparison.py
+++ trial_embedding_comparison.py
@@ -187,14 +187,12 @@ std_feature_map = total_stat_feature_map.std(axis=0)
 fig, axs = plt.subplots(4, 3, figsize=(30, 40))
 for i in range(12):
     # force to use 0 to 3 as the color bar range
-    image_to_plot = mean_feature_map[i, :, :]
-
     axs[i//3, i%3].imshow(mean_feature_map[i, :, :], vmin=0, vmax=3)
     axs[i//3, i%3].set_title(f"Block {i} mean: {mean_feature_map[i, :, :].mean():.4f}")
     axs[i//3, i%3].set_xlabel("Width")
     axs[i//3, i%3].set_ylabel("Height")
     # show the color bar
-    plt.colorbar(axs[i//3, i%3].imshow(mean_feature_map[i, :, :]))
+    plt.colorbar(axs[i//3, i%3].images[0], ax=axs[i//3, i%3])
 
 plt.savefig(root_dir+"feature_map_wise_diff_mean.png")
 plt.close()
@@ -204,14 +202,12 @@ print("Feature map wise diff saved to", root_dir+"feature_map_wise_diff_mean.png
 fig, axs = plt.subplots(4, 3, figsize=(30, 40))
 for i in range(12):
     # force to use 0 to 3 as the color bar range
-    image_to_plot = std_feature_map[i, :, :]
-
     axs[i//3, i%3].imshow(std_feature_map[i, :, :], vmin=0, vmax=1.5)
     axs[i//3, i%3].set_title(f"Block {i} std: {std_feature_map[i, :, :].mean():.4f}")   
     axs[i//3, i%3].set_xlabel("Width")
     axs[i//3, i%3].set_ylabel("Height")
     # show the color bar
-    plt.colorbar(axs[i//3, i%3].imshow(std_feature_map[i, :, :]))
+    plt.colorbar(axs[i//3, i%3].images[0], ax=axs[i//3, i%3])
 
 plt.savefig(root_dir+"feature_map_wise_diff_std.png")
 plt.close()
