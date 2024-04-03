5fe31bf360c67bdd743be0bb496b8db0d81791f4
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-03 12:56:02 -0500

    Good morning!

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,3 +3,4 @@ from .paired_MR_CT import PairedMRCTDataset_test
 from .small_hdf5 import small_hdf5_dataset
 from .small_hdf5 import slice_hdf5_dataset
 from .small_npy import slice_npy
+from .nii_dataset import simple_nifti_dataset
---
+++ dataset/nii_dataset.py
@@ -0,0 +1,31 @@
+from torch.utils.data import Dataset
+import nibabel as nib
+
+class simple_nifti_dataset(Dataset):
+    def __init__(self, case_list = []):
+        """
+        Args:
+            file_paths (list of str): List of HDF5 file paths.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.case_list = case_list
+
+    def __len__(self):
+        return len(self.case_list)
+
+    def __getitem__(self, idx):
+        folder_path = self.case_list[idx]
+        mr_path = folder_path + "/mr.nii.gz"
+        ct_path = folder_path + "/ct.nii.gz"
+        # try load both
+        try:
+            mr_file = nib.load(mr_path).get_fdata()
+            ct_file = nib.load(ct_path).get_fdata()
+        except:
+            # if failed, raise an error
+            raise ValueError(f"Error loading {mr_path} or {ct_path}")
+        
+        mr_data = mr_file.get_fdata()
+        ct_data = ct_file.get_fdata()
+        return {"mr": mr_data, "ct": ct_data}
