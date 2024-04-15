0e195a911cc1b74319772ce44bf4bea1dee09e83
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-15 14:46:22 -0500

    Good morning!

--- pre_reg_data.py
+++ pre_reg_data.py
@@ -78,7 +78,7 @@ folder_list = sorted(glob.glob(data_folder+"*/"))
 
 #     print(f"Saved resampled files to {re_ct_filename}, {re_nc_filename}")
 
-resample CT and NC to the same resolution using ANTs
+# resample CT and NC to the same resolution using ANTs
 import ants 
 
 # for folder in folder_list:
@@ -92,7 +92,6 @@ import ants
 #     nc_file = nib.load(nc_path)
 
 #     # write the affine and header to the header_affine
-#     os.system(f"rm {os.path.join(folder, 'header_affine.txt')}")
 #     with open(os.path.join(folder, "header_affine.txt"), "w") as f:
 #         f.write(f"MR affine:\n{mr_file.affine}\nMR header:\n{mr_file.header}\n\n")
 #         f.write(f"CT affine:\n{ct_file.affine}\nCT header:\n{ct_file.header}\n\n")
@@ -112,10 +111,12 @@ for folder in folder_list:
     nc_ants_img = ants.image_read(nc_path)
 
     re_ct_img = ants.resample_image(ct_ants_img, mr_ants_img, use_voxels=False, interp_type=1)
-    re_nc_img = ants.resample_image(nc_ants_img, mr_ants_img, use_voxels=False, interp_type=1)
-
     ants.image_write(re_ct_img, os.path.join(folder, "ants_reCT.nii.gz"))
+    print(f"Saved resampled CT to {os.path.join(folder, 'ants_reCT.nii.gz')}"
+
+    re_nc_img = ants.resample_image(nc_ants_img, mr_ants_img, use_voxels=False, interp_type=1)
     ants.image_write(re_nc_img, os.path.join(folder, "ants_reNC.nii.gz"))
+    print(f"Saved resampled NC to {os.path.join(folder, 'ants_reNC.nii.gz')}")
 
 # register the CT and NC to MR using ANTs
 # import ants 
